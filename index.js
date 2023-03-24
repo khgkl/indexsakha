3e44db97b45fbd0adfc196721a7e3b7ddcf16946
sardana <96410677+khgkl@users.noreply.github.com>
2023-03-24 10:54:41 +0900

    Add files via upload
    Signed-off-by: sardana <96410677+khgkl@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,145 @@
+let btn6 = document.querySelector(".btn6");
+let btn7 = document.querySelector(".btn7");
+let btn8 = document.querySelector(".btn8");
+let btn9 = document.querySelector(".btn9");
+let btn10 = document.querySelector(".btn10");
+let btn11 = document.querySelector(".btn11");
+let btn12 = document.querySelector(".btn12");
+let btn13 = document.querySelector(".btn13");
+let btn14 = document.querySelector(".btn14");
+let btn15 = document.querySelector(".btn15");
+let btn16 = document.querySelector(".btn16");
+let btn17 = document.querySelector(".btn17");
+let btn18 = document.querySelector(".btn18");
+let btn19 = document.querySelector(".btn19");
+let btn20 = document.querySelector(".btn20");
+let btn21 = document.querySelector(".btn21");
+let btn22 = document.querySelector(".btn22");
+let btn23 = document.querySelector(".btn23");
+ btn6.onclick = function(){
+ 		alert('ваша заявка отправлена');
+       btn6.style.display = 'none';
+       btn7.style.display = 'block';
+ }
+
+
+ btn7.onclick = function(){
+       btn6.style.display = 'block';
+       btn7.style.display = 'none';
+   
+ }
+
+btn8.onclick = function(){
+ 		alert('ваша заявка отправлена');
+       btn8.style.display = 'none';
+       btn9.style.display = 'block';
+ }
+
+
+ btn9.onclick = function(){
+       btn8.style.display = 'block';
+       btn9.style.display = 'none';
+   
+ }
+
+
+btn10.onclick = function(){
+ 		alert('ваша заявка отправлена');
+       btn10.style.display = 'none';
+       btn11.style.display = 'block';
+ }
+
+
+ btn11.onclick = function(){
+       btn10.style.display = 'block';
+       btn11.style.display = 'none';
+   
+ }
+
+btn12.onclick = function(){
+ 		alert('ваша заявка отправлена');
+       btn12.style.display = 'none';
+       btn13.style.display = 'block';
+ }
+
+
+ btn13.onclick = function(){
+       btn12.style.display = 'block';
+       btn13.style.display = 'none';
+   
+ }
+
+
+btn14.onclick = function(){
+ 		alert('ваша заявка отправлена');
+       btn14.style.display = 'none';
+       btn15.style.display = 'block';
+ }
+
+
+ btn15.onclick = function(){
+       btn14.style.display = 'block';
+       btn15.style.display = 'none';
+   
+ }
+
+btn16.onclick = function(){
+ 		alert('ваша заявка отправлена');
+       btn16.style.display = 'none';
+       btn17.style.display = 'block';
+ }
+
+
+ btn17.onclick = function(){
+       btn16.style.display = 'block';
+       btn17.style.display = 'none';
+   
+ }
+
+
+
+btn18.onclick = function(){
+ 		alert('ваша заявка отправлена');
+       btn18.style.display = 'none';
+       btn19.style.display = 'block';
+ }
+
+
+ btn19.onclick = function(){
+       btn18.style.display = 'block';
+       btn19.style.display = 'none';
+   
+ }
+
+
+
+btn20.onclick = function(){
+ 		alert('ваша заявка отправлена');
+       btn20.style.display = 'none';
+       btn21.style.display = 'block';
+ }
+
+
+ btn21.onclick = function(){
+       btn20.style.display = 'block';
+       btn21.style.display = 'none';
+   
+ }
+
+
+
+
+btn22.onclick = function(){
+ 		alert('ваша заявка отправлена');
+       btn22.style.display = 'none';
+       btn23.style.display = 'block';
+ }
+
+
+ btn23.onclick = function(){
+       btn22.style.display = 'block';
+       btn23.style.display = 'none';
+   
+ }
+
+
